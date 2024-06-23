d23303d0c3848467dfc34ee9ba269103b9b59800
Prajwal Labhane <144423812+PrajwalAL22@users.noreply.github.com>
2024-06-23 18:03:18 +0530

    Delete Backend directory
    Deleting Backend Directory for Deployment

--- Backend/TicketBooking/src/main/java/com/Coritech/repository/JourneyRepository.java
+++
@@ -1,9 +0,0 @@
-package com.Coritech.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.Coritech.beans.Journey;
-
-public interface JourneyRepository extends JpaRepository<Journey, Integer> {
-	Journey findByCityName(String cityName);
-}
--- Backend/TicketBooking/src/main/java/com/Coritech/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.Coritech.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.Coritech.beans.User;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-	User findByUsername(String username);
-}
